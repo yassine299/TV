78fdaf2b0ddd2da4f83b123f07d5cdd715a8f2d4
yassine299 <yassinehajjoubi299@gmail.com>
2021-12-26 18:27:41 +0100

    add list of channels

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 //start the stream when click the button
 var play = document.getElementById("stream");
+var play2 = document.getElementById("btn");
+
 
 //get the data information from api
 const url = "https://iptv-org.github.io/iptv/channels.json";
@@ -7,7 +9,7 @@ const url = "https://iptv-org.github.io/iptv/channels.json";
 fetch(url)
     .then(response => response.json())
     .then(data =>
-        //when click the button play random channel
+        //when click the button play random channel 
         play.addEventListener("click", () => {
             document.getElementById("vid").src = data[Math.floor(Math.random() * data.length)].url;
             var player = videojs('hls-example');
@@ -15,3 +17,58 @@ fetch(url)
         }
         )
     );
+
+//search for channels 
+//ps:fix the reload problem 
+
+function select() {
+    var myList = document.getElementById("myList");
+    var selected = myList.options[myList.selectedIndex].text;
+    console.log(selected)
+    fetch(url)
+        .then(response => response.json())
+        .then(data =>
+            data.map(el => {
+                if (el.name == selected) {
+                    var ch = el.url
+                    document.getElementById("vid2").src = ch;
+                    var player = videojs("get_channel");
+                    player.play();
+                    console.log(el.name + "=" + el.url)
+                }
+            })
+
+        )
+}
+
+function getchannel() {
+    window.location.reload();
+}
+
+
+//Load select button 
+const tv = []
+fetch(url)
+    .then(response => response.json())
+    .then(data =>
+        data.map(element => {
+            if (element.name.includes("")) {
+                tv.push(element.name);
+
+
+                var select = document.getElementById("myList");
+                for (var i = 0; i < tv.length; i++) {
+                    var opt = tv[i];
+                }
+
+
+                var el = document.createElement("option");
+                el.text = opt;
+                el.value = opt;
+
+                select.add(el);
+            }
+
+        }
+        )
+    )
